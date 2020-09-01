20a8ab46fce54797ab36a11b39b0720fdfeb238d
antoni-devlin <antoni.devlin@gmail.com>
2020-09-01 10:24:58 +0100

    first commit

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+
+  function theme_styles() {
+    wp_enqueue_style('theme_main_styles', get_stylesheet_uri());
+  }
+
+  add_action('wp_enqueue_scripts', 'theme_styles');
+
+  function theme_scripts() {
+    wp_enqueue_script( 'menu-js', '/wp-content/themes/slurp-theme/js/menu.js', false );
+}
+
+function register_theme_menus() {
+  register_nav_menu('sidebar-nav',__( 'Sidebar Nav' ));
+}
+add_action( 'init', 'register_theme_menus' );
+
+  add_action( 'wp_enqueue_scripts', 'theme_scripts' );
+  add_theme_support( 'post-thumbnails' ); 
+
+ ?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+    <nav class="nav-side">
+        <a href="javascript:void(0)" onclick="toggleNav()" class="nav-toggle"></a>
+        <h3><?php bloginfo() ?></h3>
+        <?php wp_nav_menu( array( 'sidebar-nav' => 'sidebar-nav', 'container' => 'ul', 'menu_class' => 'categories' ) );  ?>
+    </nav>
+    <main class="main-content">
+      <div class="grid-wrapper">
+        <?php
+          while (have_posts()) {
+            the_post(); ?>
+            <article onclick="location.href='<?php the_permalink(); ?>'" class="recipe-card">
+            <?php if (has_post_thumbnail()) { ?>
+              <img src="<?php the_post_thumbnail_url(); ?>" alt="">
+            <?php } else { ?>
+              <img src="https://i.picsum.photos/id/826/400/500.jpg?hmac=q3ZCuylWRDrInYxbqomN4WmimItkCHU9m-P1ZvLHDkw">
+           <?php } ?>
+                <h1> <?php the_title(); ?> </h1>
+                <!-- <time>2h</time> -->
+            </article>
+          <?php }
+            get_footer(); ?>
+      </div>
+        </main>
+</body>
+
+</html>
