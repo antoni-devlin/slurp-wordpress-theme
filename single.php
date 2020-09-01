39f1857f57b2e7cd09ae102703737eafbb7a28ce
antoni-devlin <antoni.devlin@gmail.com>
2020-09-01 11:00:11 +0100

    Added working single.php

--- single.php
+++ single.php
@@ -1,13 +1,25 @@
-<?php
+<?php get_header();
 
-  get_header();
+while (have_posts()) {
+  the_post(); ?>
+    <main class="main-content">
+        <header>
+        </header>
+        <div class="wrapper">
+            <h1 class="recipe-name"><?php the_title();  ?></h1>
+            <img src="<?php the_post_thumbnail_url(); ?>" alt="">
+            <h2>Ingredients</h2>
+            <ul class="ingredients" data-columns="2">
+                <li>Ingredient 1</li>
+                <li>Ingredient 2</li>
+                <li>Ingredient 3</li>
+            </ul>
+            <div class="method">
+                <h2>Method</h2>
+                <?php the_content(); ?>
+            </div>
+        </div>
+    </main>
+<?php }; 
 
-  while (have_posts()) {
-    the_post(); ?>
-    <h2><?php the_title(); ?> </h2>
-    <p> <?php the_content(); ?> </p>
-  <?php }
-
-  get_footer();
-
-?>
+get_footer();  ?>
