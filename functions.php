d467cb6f165b1c2751b107b8fc95b9c6b6fbc342
root <root@Ubuntu-20.04>
2021-02-10 14:49:52 +0000

    First commit in a while

--- functions.php
+++ functions.php
@@ -16,6 +16,23 @@ function register_theme_menus() {
 add_action( 'init', 'register_theme_menus' );
 
   add_action( 'wp_enqueue_scripts', 'theme_scripts' );
-  add_theme_support( 'post-thumbnails' ); 
+  add_theme_support( 'post-thumbnails' );
 
  ?>
+
+ <?php
+ add_action('wp_head', 'wpb_add_googleanalytics');
+ function wpb_add_googleanalytics() { ?>
+
+   <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-110803193-2"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-110803193-2');
+  </script>
+
+
+ <?php } ?>
--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <?php get_header(); ?>
-    <main class="main-content">
-      <div class="grid-wrapper">
-        <?php
-          while (have_posts()) {
-            the_post(); ?>
-            <article onclick="location.href='<?php the_permalink(); ?>'" class="recipe-card">
-            <?php if (has_post_thumbnail()) { ?>
-              <img src="<?php the_post_thumbnail_url(); ?>" alt="">
-            <?php } else { ?>
-              <img src="https://i.picsum.photos/id/826/400/500.jpg?hmac=q3ZCuylWRDrInYxbqomN4WmimItkCHU9m-P1ZvLHDkw">
-           <?php } ?>
-                <h1> <?php the_title(); ?> </h1>
-                <!-- <time>2h</time> -->
-            </article>
-          <?php }
-            get_footer(); ?>
-      </div>
-        </main>
+<main class="main-content">
+  <div class="grid-wrapper">
+    <?php
+    while (have_posts()) {
+      the_post(); ?>
+      <article onclick="location.href='<?php the_permalink(); ?>'" class="recipe-card">
+        <?php if (has_post_thumbnail()) { ?>
+          <img src="<?php the_post_thumbnail_url(); ?>" alt="">
+        <?php } else { ?>
+          <img src="https://i.picsum.photos/id/826/400/500.jpg?hmac=q3ZCuylWRDrInYxbqomN4WmimItkCHU9m-P1ZvLHDkw">
+        <?php } ?>
+        <h1> <?php the_title(); ?> </h1>
+      </article>
+    <?php }
+    get_footer(); ?>
+  </div>
+</main>
 </body>
 
 </html>
